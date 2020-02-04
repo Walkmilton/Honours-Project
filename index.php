6445aebe9db58cb4b6a09d55db09feedc66e6e59
Walkmilton <dex.doyle@me.com>
2020-02-04 18:38:41 +0000

    Adding info and warning

--- index.php
+++ index.php
@@ -78,7 +78,67 @@ $conn = new mysqli($servername, $usernameDB, $passwordDB, $dbname);
 		</form>
 
 
+		<div class="row" id="infoButton">
+	<div class="col"></div>		
+		<div class="col">
+			<button type="button" class="btn btn-info btn-block" data-toggle="modal" data-target="#infoModal">More Info</button>
+			</div>
+			<div class="col"></div>
+		</div>
+		
+		
+			<div class="toast" data-autohide="false">
+  <div class="toast-header">
+		 <strong class="mr-auto text-primary">Note:</strong>
+		<button type="button" class="ml-2 mb-1 close" data-dismiss="toast">&times;</button>
+  </div>
+  <div class="toast-body">
+    Please don't enter any personal information!
+  </div>
+</div>
+
 	</div>
+
+	<!-- The Modal -->
+<div class="modal" id="infoModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <!-- Modal Header -->
+      <div class="modal-header">
+        <h4 class="modal-title">More Info</h4>
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+
+      <!-- Modal body -->
+      <div class="modal-body">
+        Thank you for visiting my Honours Project website. 
+				<br> <br>
+				The aim of this project is to find out how people feel about computer security, what their emotions are towards it, and how it affects them. <br> <br>
+				
+				The data gathered will be used to aid cybersecurity research, affective positive change within the industry. Please don't put any personal data! <br> <br>
+				
+				If you have any questions on how this data will be used, or want more information on the project, please feel free to email me at 1600219@abertay.ac.uk <br> <br>
+				
+				Again, thank you!
+      </div>
+
+      <!-- Modal footer -->
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+
+    </div>
+  </div>
+</div>
+	
+
+	
+		<script>
+$(document).ready(function(){
+  $('.toast').toast('show');
+});
+</script>
 
 </body>
 
